981552ca5ed170958daf9d5039e728629c41fe70
Hugo Leblanc <dullin@hololink.org>
2020-10-05 07:24:28 -0400

    Client / Server / Database (Postgres)

--- server/app.js
+++ server/app.js
@@ -4,6 +4,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
 
+var pgp = require('pg-promise')(/* options */)
+var db = pgp('postgres://postgres:postgres@localhost:5432/postgres')
+// Assumes a table clicking with columns id and clicks
 
 var app = express();
 
@@ -16,14 +19,18 @@ app.use(cors());
 
 let clicks = {clicks : 0};
 
+db.one('select * from clicking where id = 1')
+.then(data => clicks.clicks = data.clicks
+)
+
 app.get('/clicks', (req, res) => {
     res.send(clicks);
 })
 
-app.post('/clicks', (req, res) => {
-    clicks.clicks++;
-    console.log(req.body)
-    res.send(clicks)
+app.post('/clicks', async (req, res) => {
+    await db.none('update clicking set clicks = clicks + 1 where id = 1')
+    const data = await db.one('select clicks from clicking where id = 1')
+    res.send(data)
 })
 
 module.exports = app;
